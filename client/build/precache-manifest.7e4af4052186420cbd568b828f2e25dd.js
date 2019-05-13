c8a4ed461be6bfb825317b8e4a3fa55b3488001e
willdk3000 <w.doucetk@gmail.com>
2019-05-12 20:31:48 -0400

    premier build pour heroku

---
+++ client/build/precache-manifest.7e4af4052186420cbd568b828f2e25dd.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "7d2653deed1e9456efc2",
+    "url": "/static/css/main.0d257942.chunk.css"
+  },
+  {
+    "revision": "7d2653deed1e9456efc2",
+    "url": "/static/js/main.bc8f05f5.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "388558696c37f8b551f8",
+    "url": "/static/js/2.59cf0233.chunk.js"
+  },
+  {
+    "revision": "c1140a017a6c808ad361c540092792a0",
+    "url": "/index.html"
+  }
+];
