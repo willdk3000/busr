fc65bb13c1ced13a3db9f537ed5286c1bd5cb803
willdk3000 <w.doucetk@gmail.com>
2020-01-12 12:14:50 -0500

    test remove helmet

--- app.js
+++ app.js
@@ -22,7 +22,8 @@ const app = express();
 //utilisation des modules
 //
 //////////////////////////
-app.use(helmet());
+
+//app.use(helmet());
 
 //CORS pour socketio
 // app.use(function (req, res, next) {
