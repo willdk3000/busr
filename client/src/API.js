83fc2f57e0b83aa1216dd52e0caeb7e1e26798dc
willdk3000 <w.doucetk@gmail.com>
2020-01-12 12:19:39 -0500

    change socket url

--- client/src/API.js
+++ client/src/API.js
@@ -6,7 +6,7 @@ import io from "socket.io-client";
 //   io.connect('busr-mtl.herokuapp.com', { transports: ['websocket'] }) :
 //   io.connect('http://localhost:5000', { transports: ['websocket'] })
 
-const socket = io.connect('http://localhost:5000', { transports: ['websocket'] })
+const socket = io.connect('http://192.168.0.174:5000', { transports: ['websocket'] })
 
 // Fonctions realtime
 
