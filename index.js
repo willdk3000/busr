fc929467ced438c4c04ebc780193388bae4ebfa6
willdk3000 <w.doucetk@gmail.com>
2020-01-12 12:23:13 -0500

    change socket url

--- index.js
+++ index.js
@@ -6,8 +6,4 @@ const server = http.Server(app);
 sockets.init(server);
 
 const port = process.env.PORT || 5000;
-server.listen(port, "0.0.0.0", () => {
-  /* eslint-disable no-console */
-  console.log(`Listening: http://localhost:${port}`);
-  /* eslint-enable no-console */
-});
+server.listen(port, "0.0.0.0");
